1cdea962101810b88fe40491e5089f148bc31442
Anoop G <91954886+anoopg1@users.noreply.github.com>
2024-01-29 14:37:36 +0530

    splash screen completed

---
+++ lib/constants/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+BuildContext? context;
+
+final currentHeight = MediaQuery.sizeOf(context!).height;
+
+const kheight5 = SizedBox(
+  height: 5,
+);
+
+const kheight10 = SizedBox(
+  height: 10,
+);
+
+const kwidth5 = SizedBox(
+  width: 5,
+);
+
+const kwidth10 = SizedBox(
+  width: 10,
+);
